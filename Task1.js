8f6385c4d411c6cc9ef8b3669cd77d7fe3a20f5b
Ghada Lotfi <g.f.lotfi@gmail.com>
2022-03-02 14:13:49 +0200

    Update Task1.js

--- Task1.js
+++ Task1.js
@@ -9,19 +9,32 @@ var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperi
         if (error) { 
             console.log('Unable to connect to Forecast API'); 
        		 } 
+				
           else { 
-
+			  try{
+		var test = response;
+	//	console.log(response.body.main);
 		var temp = response.body.main.temp 
+		var new1 = response.body.main.humidity
 		var obj = {"name: temperature" : "value:" + temp }
+		var obj2 = {"name: humidity" : "value:" + new1 }
 		var s = JSON.stringify(obj)
+		var s2 = JSON.stringify(obj2)
 		var output = JSON.parse(s)
+		var output2 = JSON.parse(s2)
 
-            	console.log(response.body.main.temp); 
+            	console.log(response.body.main.humidity); console.log(output2); 
   
-           	return output;
-       		 } 
+           	return output2;
+       		 
+			}
+			catch(error){
+				console.log("ERROR")
+			}
+		}
     	}) 
 } 
+
 
 const prompt = require("prompt-sync")({ sigint: true });
 const country = prompt("Country? ");
